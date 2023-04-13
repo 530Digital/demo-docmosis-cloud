6199f7c97e6b066572010f6e2560939bb4a71198
Josh Martinez <josh@530digital.com>
2023-04-13 16:39:11 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Demo Docmosis Cloud
+
+[Docmosis Cloud](https://www.docmosis.com/products/cloud) is a cloud document generation API.
+
+This demo is to get hands on with Docmosis Cloud.
+
+## Requirements
+
+- Node 18.15.0
+- Docmosis Cloud Account & API Key
+- [Understanding of Docmosis templates](https://resources.docmosis.com/search?issearch=1&theme=FilterList&isc=1&ordering=zelevance&category_id=2&searchword=Cloud)
+
+### Getting Started
+
+- Clone this repo to your machine
+- `cd` into repo directory
+- Rename .example.env to .env
+- Log into your Docmosis Cloud account and get your API Key and update the .env file
+- Run: `npm i`
+- Upload the template located in ./templates to your Docmosis Cloud account temlate directory
+
+#### Generating Documents
+
+- Run: `node renderDocument.js`
+- Open the file generated in the `output` directory
+
+#### Other
+
+- [Docmosis Security Statement](https://www.docmosis.com/products/cloud/#security-statement)
+- [Example Templates](https://resources.docmosis.com/search?issearch=1&theme=FilterList&isc=1&ordering=zelevance&category_id=3)
+- [Cloud (DWS3) - Web Services Guide](https://resources.docmosis.com/content/documentation/cloud-dws3-web-services-guide)
---
+++ test-data/data.js
@@ -0,0 +1,71 @@
+module.exports = {
+  invoiceNo: "23423",
+  invoiceDate: "07 Apr 2023",
+  purchaseOrder: "3KWELK783K",
+  customerCompany: "530Digital",
+  customerContact: "Josh Martinez",
+  customerAddr1: "123 Street",
+  customerAddr2: "",
+  customerCity: "Redding",
+  customerState: "CA",
+  customerZIP: "96002",
+  terms: "30",
+  items: [
+    {
+      qty: "3",
+      ItemName: "ACP101 Accounting Package",
+      itemDescription:
+        "Annual Subscription to Premier Version with Tax, Inventory and Payroll Plugins",
+      amt: "1350.00",
+    },
+    {
+      qty: "4.5",
+      ItemName: "ACP101T Online Training",
+      itemDescription:
+        "Hours of Training in Premier Version - Interactive Demos with Q&A Sessions",
+      amt: "495.00",
+    },
+    {
+      qty: "10",
+      ItemName: "ACP101S Standard Support",
+      itemDescription:
+        "Initial Hours allocated for access to email and phone support for Premier Version",
+      amt: "1100.00",
+    },
+    {
+      qty: "6",
+      ItemName: "ACP101C Screen Customization",
+      itemDescription:
+        "Hours spent customizing screens in Premier Version for client requirements",
+      amt: "660.00",
+    },
+    {
+      qty: "4.5",
+      ItemName: "ACP101R Report Customization",
+      itemDescription:
+        "Hours spent customizing reports in Premier Version for client requirements",
+      amt: "495.00",
+    },
+    {
+      qty: "2",
+      ItemName: "ACP101I System Imports",
+      itemDescription:
+        "Hours spent importing customer records into Premier Version",
+      amt: "220.00",
+    },
+    {
+      qty: "3",
+      ItemName: "ACP100 Accounting Package",
+      itemDescription:
+        "Annual Subscription to Standard Version of Accounts System",
+      amt: "900.00",
+    },
+    {
+      qty: "4.5",
+      ItemName: "ACP100T Online Training",
+      itemDescription:
+        "Hours of Training in Standard Version - Interactive Demos with Q&A Sessions",
+      amt: "495.00",
+    },
+  ],
+};
